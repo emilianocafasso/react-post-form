33f67664e794adc4f43419095477e704c44ab716
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-17 00:43:09 +0100

    initialize formData state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
 
-  const [formData, setFormData] = useState()
+  const [formData, setFormData] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: 'false' //booleano - checkbox
+  })
 
 
   return (
